e5e6c1d7cc1aa1f76c0e550941c90d20b892842b
rgary <gary.remy@gmail.com>
2022-03-20 12:13:14 +0100

    p

--- src/app/services/data-storage.service.ts
+++ src/app/services/data-storage.service.ts
@@ -1,16 +1,16 @@
-import {Injectable, OnDestroy} from '@angular/core';
+import {Injectable} from '@angular/core';
 import {LocalStorageService} from "./local-storage.service";
 import {PersonService} from "./person.service";
-import {TripService, TripToJSON} from "./trip.service";
+import {TripService} from "./trip.service";
 import {SpendingService} from "./spending.service";
 import {RefundService} from "./refund.service";
-import { interval, Subscription, BehaviorSubject } from 'rxjs';
-import {HttpClient, HttpHeaders} from "@angular/common/http";
+import { BehaviorSubject } from 'rxjs';
+import {DataAccessService} from "./data-access.service";
 
 @Injectable({
   providedIn: 'root'
 })
-export class DataStorageService implements OnDestroy {
+export class DataStorageService {
 
   private participant_list_key: string = "PARTICIPANT_LIST";
   private trip_info_key: string = "TRIP_INFO";
@@ -19,27 +19,13 @@ export class DataStorageService implements OnDestroy {
   public tripBehaviorSubject: BehaviorSubject<TripService> = new BehaviorSubject<TripService>(new TripService());
   private trip: TripService;
 
-  public static BACKEND_URL:string ;
-
-  subs: Subscription;
-
   constructor(private localStorage: LocalStorageService,
-              private http: HttpClient,) {
-    if (window.location.href.indexOf("localhost") === -1 && window.location.href.indexOf("127.0.0.1") === -1) {
-      DataStorageService.BACKEND_URL = 'http://serversuccu.serveftp.com:3000';
-    } else {
-      DataStorageService.BACKEND_URL = 'http://127.0.0.1:3000';
-    }
+              private access: DataAccessService) {
+
     this.trip = new TripService();
     this.tripBehaviorSubject.next(this.getTrip());
-    console.log("BACKEND URL C: %o", DataStorageService.BACKEND_URL);
 
-    const source = interval(20000);
-    this.subs = source.subscribe(n => this.retrieveTripFromServer(this.trip.uuid));
-  }
-
-  ngOnDestroy(): void {
-    this.subs.unsubscribe();
+    this.access.dataObservable.subscribe(value => this.setTrip(value));
   }
 
   getParticipantList(): PersonService[] {
@@ -50,29 +36,18 @@ export class DataStorageService implements OnDestroy {
     return [];
   }
 
-  private createTrip(): TripService {
-    console.log("New Trip altogether");
-    const headers = new HttpHeaders().set('Content-Type', 'application/json; charset=utf-8');
-    this.http.post<TripService>(DataStorageService.BACKEND_URL+"/trip/create", TripToJSON(this.trip), {headers: headers}).subscribe(data => {
-      console.log("Called for create trip. Got %o", data);
-      this.setTrip(data);
-    });
-    return this.trip;
-  }
-
   private getTrip(): TripService {
     if (!this.updated) {
-      console.log("Not updated: %o", this.trip);
       return this.trip;
     }
     this.updated = false;
     let string_trip = this.localStorage.get(this.trip_info_key);
     if (string_trip) {
-      console.log("Trip from storage");
       this.trip = JSON.parse(string_trip);
       return this.trip;
     }
-    return this.createTrip();
+    this.access.createTrip(this.trip);
+    return this.trip;
   }
 
   setParticipantList(list: PersonService[]): void {
@@ -94,7 +69,6 @@ export class DataStorageService implements OnDestroy {
     trip.number_of_refunds_on_participants = number_of_refunds_on_participants;
 
     this.localStorage.set(this.trip_info_key, JSON.stringify(trip));
-    console.log("Stored value: %o", trip);
     this.tripBehaviorSubject.next(trip);
     this.updated = true;
   }
@@ -145,7 +119,7 @@ export class DataStorageService implements OnDestroy {
 
   reset():void {
     this.updated = true;
-    this.setTrip(this.createTrip());
+    this.access.createTrip(new TripService());
   }
 
   refresh():void {
@@ -154,16 +128,5 @@ export class DataStorageService implements OnDestroy {
     this.tripBehaviorSubject.next(this.trip);
   }
 
-  retrieveTripFromServer(uuid: string): void {
-    this.http.get<TripService>(DataStorageService.BACKEND_URL+"/trip/"+uuid, { observe: 'response' }).subscribe(data => {
-      console.log("Full Response: %o", data);
-      if (data.status === 404) {
-        console.log("Trip do not exists in DB.")
-        return;
-      }
-      console.log("Called for retrieve trip. Got %o", data);
-      this.setTrip(data.body!);
-    });
-  }
 
 }
